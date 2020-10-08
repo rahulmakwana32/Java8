ffb618a99364aeb3d30a23456f2e2307ca96a1d1
Rahul Makwana <rahulmakwana32@gmail.com>
2020-10-07 23:33:04 -0700

    Optiona Java Class

---
+++ OptionalJava/src/Optionals.java
@@ -0,0 +1,19 @@
+import java.util.Optional;
+
+public class Optionals {
+	
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		
+		
+		Integer m=null;
+		Optional<Integer> k = Optional.empty();
+		
+		k = Optional.ofNullable(null);
+		
+		System.out.println(k.toString());
+		System.out.println(m.toString());
+		
+	}
+
+}
